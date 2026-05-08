aa7df4bf5238b6d4b64cc49092d28c5c0aa5538a
nicoki2004 <nicoki2004@gmail.com>
2026-05-08 17:05:31 -0300

    change api/metrics for admin/metrics. and change plain text to html response

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -2,6 +2,12 @@ import type { Request, Response } from "express";
 import { config } from "../config.js";
 
 export async function handlerMetrics(_: Request, res: Response) {
-	res.send(`Hits: ${config.fileserverHits}`);
-}
 
+	res.set("Content-Type", "text/html; charset=utf-8");
+	res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`)
+}
